4f294999108af35cbf015afc575d546dbf0955e2
Syed Musawer Mehdi <musawer.mehdi2@gmail.com>
2023-09-26 18:36:30 +0500

    Project Setup

---
+++ Domain/Exceptions/BaseException.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Common;
+using System;
+
+namespace Domain.Exceptions;
+
+public class BaseException : Exception
+{
+    public HttpStatusCode StatusCode { get; }
+
+    public BaseException(HttpStatusCode statusCode)
+    {
+        StatusCode = statusCode;
+    }
+
+    public BaseException(HttpStatusCode statusCode, string message)
+    : base(message)
+    {
+        StatusCode = statusCode;
+    }
+}
